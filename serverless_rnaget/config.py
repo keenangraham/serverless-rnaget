589d109da64df413444a6c0ff33dd91302a4fc6d
Keenan Graham <keenangraham@stanford.edu>
2021-10-29 19:05:46 -0700

    Add custom domain

--- serverless_rnaget/config.py
+++ serverless_rnaget/config.py
@@ -3,6 +3,8 @@ config = {
         'vpc_id': 'vpc-ea3b6581',
         'security_group_id': 'sg-022ea667',
         'elasticsearch': 'https://vpc-rna-expression-dro56qntagtgmls6suff2m7nza.us-west-2.es.amazonaws.com',
+        'domain_certificate_arn': 'arn:aws:acm:us-west-2:618537831167:certificate/6e16fc50-1206-48fa-b14a-13d97cb1fee6',
+        'domain_name': 'api.encodedcc.org',
     },
     'account': '618537831167',
     'region': 'us-west-2',
